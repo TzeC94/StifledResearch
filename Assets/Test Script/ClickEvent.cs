a0d118eefe57c29056c0d76048b2a569d7c997d6
Teoh Tze Chian <teoh_0827@hotmail.com>
2020-06-26 21:13:24 +0800

    Initial Commit #5

---
+++ Assets/Test Script/ClickEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ClickEvent : MonoBehaviour {
+
+    public UnityEvent targetEvent;
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(Input.GetMouseButtonDown(0)) {
+
+            targetEvent.Invoke();
+
+        }
+        	
+	}
+}
